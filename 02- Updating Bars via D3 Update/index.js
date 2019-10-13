777ab972b1e25640b2bbc176de7e536f5a4ead75
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-10-13 23:43:01 +0500

    Update Transition

--- 02- Updating Bars via D3 Update/index.js
+++ 02- Updating Bars via D3 Update/index.js
@@ -1,6 +1,10 @@
 // select the svg container first
 const svg = d3.select('.canvas')
+<<<<<<< HEAD
     .append('svg')
+=======
+  .append('svg')
+>>>>>>> cd823fd... 02- Fetching Data from Firestore
     .attr('width', 600)
     .attr('height', 600);
 
@@ -10,6 +14,7 @@ const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
+<<<<<<< HEAD
     .attr('width', graphWidth)
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
@@ -89,5 +94,74 @@ db.collection('dishes').get().then(res => {
         data.pop();
         update(data)
     }, 1000);
+=======
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)
+
+const yAxisGroup = graph.append('g');
+
+db.collection('dishes').get().then(res => {
+
+
+
+  var data = [];
+  res.docs.forEach(doc => {
+    console.log(doc.data())
+    data.push(doc.data());
+  });
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.orders)])
+    .range([graphHeight, 0]);
+  //
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, graphWidth])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+  // join the data to circs
+  const rects = graph.selectAll('rect')
+    .data(data);
+
+  console.log(rects)
+  rects.exit().remove();
+
+  // add attrs to circs already in the DOM
+  rects.attr('width', x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+  // append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+      .attr('width', x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      .attr('y', d => y(d.orders));
+
+  // create & call axesit
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  xAxisGroup.selectAll('text')
+    .attr('fill', 'orange')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+>>>>>>> cd823fd... 02- Fetching Data from Firestore
 
 });
