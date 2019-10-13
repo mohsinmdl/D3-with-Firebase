ff6524aeb331def706eacca378dffa9cda730c2d
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-10-14 00:27:24 +0500

    03- Realtime Data refresh

--- 02- Updating Bars via D3 Update/index.js
+++ 02- Updating Bars via D3 Update/index.js
@@ -14,40 +14,48 @@ const graph = svg.append('g')
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-
 // create axes groups
 const xAxisGroup = graph.append('g')
     .attr('transform', `translate(0, ${graphHeight})`);
 
+xAxisGroup.selectAll('text')
+    .attr('fill', 'orange')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end');
+
 const yAxisGroup = graph.append('g');
 
-db.collection('dishes').get().then(res => {
+const y = d3.scaleLinear()
+    .range([graphHeight, 0]);
 
+const x = d3.scaleBand()
+    .range([0, graphWidth])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
-    var data = [];
-    res.docs.forEach(doc => {
-        console.log(doc.data());
-        data.push(doc.data());
-    });
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
 
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.orders)])
-        .range([graphHeight, 0]);
-    //
-    const x = d3.scaleBand()
-        .domain(data.map(item => item.name))
-        .range([0, graphWidth])
-        .paddingInner(0.2)
-        .paddingOuter(0.2);
+// the update function
+const update = (data) => {
 
     // join the data to circs
     const rects = graph.selectAll('rect')
         .data(data);
 
     console.log(rects);
+
+    // remove unwanted rects
     rects.exit().remove();
 
-    // add attrs to circs already in the DOM
+    // update the domains
+    y.domain([0, d3.max(data, d => d.orders)]);
+    x.domain(data.map(item => item.name));
+
+    // add attrs to rects already in the DOM
     rects.attr('width', x.bandwidth)
         .attr("height", d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
@@ -63,18 +71,23 @@ db.collection('dishes').get().then(res => {
         .attr('x', (d) => x(d.name))
         .attr('y', d => y(d.orders));
 
-    // create & call axesit
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y)
-        .ticks(3)
-        .tickFormat(d => d + ' orders');
-
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
 
-    xAxisGroup.selectAll('text')
-        .attr('fill', 'orange')
-        .attr('transform', 'rotate(-40)')
-        .attr('text-anchor', 'end')
+};
+
+db.collection('dishes').get().then(res => {
+
+    var data = [];
+    res.docs.forEach(doc => {
+        data.push(doc.data());
+    });
+
+    update(data);
+
+    d3.interval(() => {
+        data.pop();
+        update(data)
+    }, 1000);
 
 });
