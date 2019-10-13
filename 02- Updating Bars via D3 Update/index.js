5d49c4ba9b486dc39b5abdc8cea7fa7b34390de8
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-10-13 23:46:01 +0500

    01- Updating Pattern

--- 02- Updating Bars via D3 Update/index.js
+++ 02- Updating Bars via D3 Update/index.js
@@ -1,6 +1,6 @@
 // select the svg container first
 const svg = d3.select('.canvas')
-  .append('svg')
+    .append('svg')
     .attr('width', 600)
     .attr('height', 600);
 
@@ -10,72 +10,71 @@ const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
-  .attr('width', graphWidth)
-  .attr('height', graphHeight)
-  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 
 // create axes groups
 const xAxisGroup = graph.append('g')
-  .attr('transform', `translate(0, ${graphHeight})`)
+    .attr('transform', `translate(0, ${graphHeight})`);
 
 const yAxisGroup = graph.append('g');
 
 db.collection('dishes').get().then(res => {
 
 
+    var data = [];
+    res.docs.forEach(doc => {
+        console.log(doc.data());
+        data.push(doc.data());
+    });
 
-  var data = [];
-  res.docs.forEach(doc => {
-    console.log(doc.data())
-    data.push(doc.data());
-  });
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.orders)])
+        .range([graphHeight, 0]);
+    //
+    const x = d3.scaleBand()
+        .domain(data.map(item => item.name))
+        .range([0, graphWidth])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
 
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.orders)])
-    .range([graphHeight, 0]);
-  //
-  const x = d3.scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, graphWidth])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+    // join the data to circs
+    const rects = graph.selectAll('rect')
+        .data(data);
 
-  // join the data to circs
-  const rects = graph.selectAll('rect')
-    .data(data);
+    console.log(rects);
+    rects.exit().remove();
 
-  console.log(rects)
-  rects.exit().remove();
+    // add attrs to circs already in the DOM
+    rects.attr('width', x.bandwidth)
+        .attr("height", d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
 
-  // add attrs to circs already in the DOM
-  rects.attr('width', x.bandwidth)
-    .attr("height", d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
+    // append the enter selection to the DOM
+    rects.enter()
+        .append('rect')
+        .attr('width', x.bandwidth)
+        .attr("height", d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', (d) => x(d.name))
+        .attr('y', d => y(d.orders));
 
-  // append the enter selection to the DOM
-  rects.enter()
-    .append('rect')
-      .attr('width', x.bandwidth)
-      .attr("height", d => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name))
-      .attr('y', d => y(d.orders));
+    // create & call axesit
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => d + ' orders');
 
-  // create & call axesit
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + ' orders');
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
-
-  xAxisGroup.selectAll('text')
-    .attr('fill', 'orange')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
+    xAxisGroup.selectAll('text')
+        .attr('fill', 'orange')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
 
 });
